c281af64fc5526e4d7140ab059322dd20b4ad759
Palash Jyoti Goswami <palashjyotigoswamipt@gmail.com>
2023-10-18 14:07:56 +0530

    changed

--- application.py
+++ application.py
@@ -1,69 +1,84 @@
-from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS, cross_origin
+from flask import Flask, render_template, request,jsonify
+from flask_cors import CORS,cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 
-application = Flask(__name__)
+application = Flask(__name__) # initializing a flask app
 app=application
 
-@app.route("/",methods=['GET'])
+@app.route('/',methods=['GET'])  # route to display the home page
 @cross_origin()
-def homepage():
-    return render_template('index.html')
+def homePage():
+    return render_template("index.html")
 
-@app.route('/review',methods=['POST', 'GET'])
+@app.route('/review',methods=['POST','GET']) # route to show the review comments in a web UI
 @cross_origin()
 def index():
-    if request.method=='POST':
+    if request.method == 'POST':
         try:
-            searchString=request.form['content'].replace(" ","")
+            searchString = request.form['content'].replace(" ","")
             flipkart_url = "https://www.flipkart.com/search?q=" + searchString
-            response = requests.get(flipkart_url)
-            flipkartpage=response.text
-            flipkart_html = bs(flipkartpage,"html.parser")
-            bigboxes = flipkart_html.find_all("div",{"class":"_1AtVbE col-12-12"})
+            uClient = uReq(flipkart_url)
+            flipkartPage = uClient.read()
+            uClient.close()
+            flipkart_html = bs(flipkartPage, "html.parser")
+            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
             del bigboxes[0:3]
-            box=bigboxes[0]
-            productlink = "https://www.flipkart.com" + box.div.div.div.a['href']
-            productreq = requests.get(productlink)
-            productreq.encoding='utf-8'
-            product_html = bs(productreq.text,"html.parser")
-            comment_boxes = product_html.find_all("div",{"class":"_16PBlm"})
-            
-            filename=searchString+'.csv'
-            fw=open(filename,'w')
-            headers="Product, Customer Name, Rating, Heading, Comment \n"
+            box = bigboxes[0]
+            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
+            prodRes = requests.get(productLink)
+            prodRes.encoding='utf-8'
+            prod_html = bs(prodRes.text, "html.parser")
+            print(prod_html)
+            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
+
+            filename = searchString + ".csv"
+            fw = open(filename, "w")
+            headers = "Product, Customer Name, Rating, Heading, Comment \n"
             fw.write(headers)
-            reviews=[]
-            for comment_box in comment_boxes:
+            reviews = []
+            for commentbox in commentboxes:
                 try:
-                    name=comment_box.div.div.find_all("p",{"class":"_2sc7ZR _2V5EHH"})[0].text
+                    #name.encode(encoding='utf-8')
+                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+
                 except:
-                    name='No Name'
-                
+                    name = 'No Name'
+
                 try:
-                    rating=comment_box.div.div.div.div.text
+                    #rating.encode(encoding='utf-8')
+                    rating = commentbox.div.div.div.div.text
+
+
                 except:
-                    rating='No Rating'
-                    
+                    rating = 'No Rating'
+
                 try:
-                    commentHead=comment_box.div.div.p.text
+                    #commentHead.encode(encoding='utf-8')
+                    commentHead = commentbox.div.div.div.p.text
+
                 except:
-                    commentHead="No Comment Heading"
-                    
+                    commentHead = 'No Comment Heading'
                 try:
-                    custComment=comment_box.div.div.find_all("div",{"class":""})[1].text
+                    comtag = commentbox.div.div.find_all('div', {'class': ''})
+                    #custComment.encode(encoding='utf-8')
+                    custComment = comtag[0].div.text
                 except Exception as e:
                     print("Exception while creating dictionary: ",e)
-                    
-                mydict={'Product':searchString, 'Name':name, 'Rating':rating, 'CommentHead':commentHead, 'Comment':custComment}
+
+                mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
+                          "Comment": custComment}
                 reviews.append(mydict)
-                
-            return render_template('results.html',reviews=reviews[0:(len(reviews)-1)])
+            return render_template('results.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
-            print("Error occurred: ",e)
+            print('The Exception message is: ',e)
+            return 'something is wrong'
+    # return render_template('results.html')
 
+    else:
+        return render_template('index.html')
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=8000,debug=True)
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8000, debug=True)
+	#app.run(debug=True)
