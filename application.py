f9ca02463eac40bf1c86f6cbcf8125552372003d
Palash Jyoti Goswami <palashjyotigoswamipt@gmail.com>
2023-10-18 17:23:52 +0530

    changed

--- application.py
+++ application.py
@@ -19,9 +19,7 @@ def index():
         try:
             searchString = request.form['content'].replace(" ","")
             flipkart_url = "https://www.flipkart.com/search?q=" + searchString
-            uClient = uReq(flipkart_url)
-            flipkartPage = uClient.read()
-            uClient.close()
+            flipkartPage = requests.get(flipkart_url)
             flipkart_html = bs(flipkartPage, "html.parser")
             bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
             del bigboxes[0:3]
